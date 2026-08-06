26ca5eb047e4c1c8bd9736ec193df80c1123f94c
cz2925928087 <2925928087@qq.com>
2026-08-06 10:23:40 +0800

    feat: add spring-cloud-gateway modules (gateway, order-service, product-service, product-api)

---
+++ spring-cloud-gateway/order-service/src/main/java/com/order/demo/controller/OrderController.java
@@ -0,0 +1,20 @@
+package com.order.demo.controller;
+
+import com.order.demo.model.OrderInfo;
+import com.order.demo.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/order")
+@RestController
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @RequestMapping("/{orderId}")
+    public OrderInfo getOrderById(@PathVariable("orderId") Integer orderId){
+        return orderService.selectOrderById(orderId);
+    }
+}
---
+++ spring-cloud-gateway/order-service/src/main/java/com/order/demo/service/OrderService.java
@@ -0,0 +1,32 @@
+package com.order.demo.service;
+
+
+import com.order.demo.mapper.OrderMapper;
+import com.order.demo.model.OrderInfo;
+
+import com.productapi.demo.api.ProductApi;
+import com.productapi.demo.model.ProductInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class OrderService {
+    @Autowired
+    private ProductApi productApi;
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public OrderInfo selectOrderById(Integer orderId){
+        OrderInfo orderInfo = orderMapper.selectOrderById(orderId);
+//        String url = "http://product-service/product/"+orderInfo.getProductId();
+//        ProductInfo productInfo = restTemplate.getForObject(url, ProductInfo.class);
+        ProductInfo productInfo = productApi.getProductInfo(orderInfo.getProductId());
+        orderInfo.setProductInfo(productInfo);
+        return orderInfo;
+    }
+}
