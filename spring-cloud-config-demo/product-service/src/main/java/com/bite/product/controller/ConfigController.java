f1f026bb7fa5d89b2737ef03106b9055cb857333
cz2925928087 <2925928087@qq.com>
2026-08-12 23:36:22 +0800

    feat: add config client and bootstrap for product-service

---
+++ spring-cloud-config-demo/product-service/src/main/java/com/bite/product/controller/ConfigController.java
@@ -0,0 +1,19 @@
+package com.bite.product.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RefreshScope
+@RequestMapping("/config")
+@RestController
+public class ConfigController {
+    @Value("${data.env}")
+    private String env;
+    @RequestMapping("/getEnv")
+    public String getEnv(){
+        return "env:"+ env;
+    }
+}
